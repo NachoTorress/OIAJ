bd4639a8da26591da5a8e7b291dbdb2fab922fa3
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2024-07-30 10:52:55 -0300

    Add files via upload

---
+++ red.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll int 
+#define nmax 1000002
+using namespace std;
+vector <vector<ll>> comp;
+vector <ll> ID;
+void unionFind (ll a, ll b){
+	ll x=ID[a], y=ID[b];
+	if (x==y) return;
+	if (comp[x].size()<comp[y].size()) swap(x,y);
+	for (auto i:comp[y]){
+		ID[i]=x;
+		comp[x].push_back(i);
+	}
+}
+int main(){
+	freopen("red.in", "r", stdin);
+	freopen("red.out", "w", stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	char eleccion;
+	cin>>eleccion;
+	string respuesta="";
+	comp.resize(nmax), ID.resize(nmax);
+	for (int i=1;i<nmax;i++){
+		comp[i].push_back(i);
+		ID[i]=i;
+	}
+	ll k1,k2;
+	while (eleccion!='F'){
+		cin>>k1>>k2;
+		if (eleccion=='P'){
+	 		respuesta+= (ID[k1]==ID[k2])? "S \n": "N \n";
+		}
+		else{
+			unionFind(k1,k2);
+		}
+		cin>>eleccion;
+	}
+	cout<<respuesta;
+}
